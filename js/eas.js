9bf212f51a3ea20e9d7c7e408c4f22b0befa0f2b
nullzeal <nullzeal@pm.me>
2022-06-20 14:56:58 -0400

    columns and rows generate 16x16 grid successfully

--- js/eas.js
+++ js/eas.js
@@ -1,32 +1,36 @@
 const rowN = 16;
-const colN = 16;
+const cellN = 16;
 
 for(let i = 0; i<rowN; i++){
     addNewRow();
 }
+
+addColsToRows();
 
 function addNewRow(){
 
     const grid = document.querySelector(".grid");
 
     const newRow = document.createElement('div');
-    newRow.style.width = '100px';
-    newRow.style.height = '100px';
-    newRow.style.backgroundColor = 'blue';
-    newRow.setAttribute('class', 'gridRow')
-
+    newRow.setAttribute('class', 'gridRow');
     grid.appendChild(newRow);
 }
-
-addColsToRows();
 
 function addColsToRows(){
     const rowList = document.getElementsByClassName('gridRow');
 
     for(let row of rowList){
-        addNewCol(row);
+        for(i=0;i<cellN;i++){
+            addNewCol(row);
+        }
     }
 
+}
+
+function addNewCol(row){
+    const newCol = document.createElement('div');
+    newCol.setAttribute('class', 'gridCell')
+    row.appendChild(newCol);
 }
 
 
