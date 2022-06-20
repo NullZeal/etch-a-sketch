ccaefd51c978d9a61824e91c0ae904bf4d2e177f
nullzeal <nullzeal@pm.me>
2022-06-20 17:34:00 -0400

    grid in now resizable with a button

--- js/eas.js
+++ js/eas.js
@@ -1,11 +1,54 @@
-const rowN = 16;
-const cellN = 16;
+var rowN = 16;
+var cellN = 16;
+var mouseDown = 0;
 
-for(let i = 0; i<rowN; i++){
-    addNewRow();
+document.body.onmousedown = function() { 
+    mouseDown=1;
+}
+document.body.onmouseup = function() {
+    mouseDown = 0;
 }
 
-addColsToRows();
+drawBoard(rowN, cellN);
+
+const button = document.querySelector('#button');
+
+button.addEventListener('click', ()=>{
+
+    let rowAmount = 0;
+    while (isNaN(rowAmount) || rowAmount<1 || rowAmount>100){
+        rowAmount = prompt('Amount of rows? (max 100)');
+        Math.round(rowAmount) 
+    }
+        
+    let columnAmount = 0;
+    while(isNaN(columnAmount) || columnAmount<1 || columnAmount>100){
+        columnAmount = prompt('Amount of cells per row? (max 100)');
+        Math.round(rowAmount) 
+    }
+
+
+    rowN = rowAmount;
+    cellN = columnAmount;
+    eraseBoard();
+    drawBoard(rowN, cellN);
+})
+
+function drawBoard(rowN, cellN){
+    for(let i = 0; i<rowN; i++){
+        addNewRow();
+    }
+    addColsToRows(cellN);
+}
+
+function eraseBoard(){
+    let grid = document.getElementById('grid')
+    while (grid.firstChild) {
+      grid.removeChild(grid.lastChild);
+    }
+    let newWidth = cellN * 15;
+    grid.style.width = `${newWidth}px`;
+}
 
 function addNewRow(){
 
@@ -16,21 +59,25 @@ function addNewRow(){
     grid.appendChild(newRow);
 }
 
-function addColsToRows(){
+function addColsToRows(cellN){
     const rowList = document.getElementsByClassName('gridRow');
-
     for(let row of rowList){
         for(i=0;i<cellN;i++){
             addNewCol(row);
         }
     }
-
 }
 
 function addNewCol(row){
     const newCol = document.createElement('div');
-    newCol.setAttribute('class', 'gridCell')
+    newCol.setAttribute('class', 'gridCell');
+
+    newCol.addEventListener('mouseenter', () => {
+        if(mouseDown==1){
+            newCol.style.backgroundColor = 'red';
+        }
+            
+    })
     row.appendChild(newCol);
 }
-
 
